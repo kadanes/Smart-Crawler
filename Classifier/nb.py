4ab611978d8092f220b6a62ac8eda842045fb6c7
Parth Tamane <parthv21@gmail.com>
2019-04-18 14:29:55 +0530

    Cleaned classifier files

--- Classifier/nb.py
+++ Classifier/nb.py
@@ -6,7 +6,7 @@ import pandas, xgboost, numpy, textblob, string
 import pickle
 
 # load the dataset
-data = open('webpagetraining1.csv', encoding="utf-8").read()
+data = open('webpagetraining.csv', encoding="utf-8").read()
 labels, texts = [], []
 lines=data.split("\n")
 for line in lines:
@@ -28,12 +28,7 @@ encoder = preprocessing.LabelEncoder()
 train_y = encoder.fit_transform(train_y)
 valid_y = encoder.fit_transform(valid_y)
 
-# create a count vectorizer object 
-#count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
-#count_vect.fit(trainDF['text'])
-
-# transform the training and validation data using count vectorizer object
-
+# transform the training and validation data using stemmed count vectorizer object
 stemmer = SnowballStemmer("english", ignore_stopwords=True)
 
 class StemmedCountVectorizer(CountVectorizer):
@@ -45,13 +40,10 @@ stemmed_count_vect = StemmedCountVectorizer(stop_words='english')
 
 xtrain_count =  stemmed_count_vect.fit_transform(train_x)
 xvalid_count =  stemmed_count_vect.transform(valid_x)
-#print(stemmed_count_vect.vocabulary)
-#print(xtrain_count.shape)
 
 tfidf_transformer = TfidfTransformer()
 xtrain_tfidf = tfidf_transformer.fit_transform(xtrain_count)
 xvalid_tfidf = tfidf_transformer.fit_transform(xvalid_count)
-#print(xtrain_tfidf.shape)
 
 # create a classifier model
 classifier=naive_bayes.MultinomialNB(fit_prior=False)
@@ -62,8 +54,3 @@ classifier.fit(xtrain_tfidf, train_y)
 # predict the labels on validation dataset
 predictions = classifier.predict(xvalid_tfidf)
 print("Accuracy: ", metrics.accuracy_score(predictions, valid_y))
-
-#pickle
-'''pickle.dump(stemmed_count_vect.vocabulary_, open("vectorizer_vocabulary.pickel", "wb"))
-pickle.dump(tfidf_transformer, open("tfidf_transformer.pickel", "wb"))
-pickle.dump(classifier, open("classifier.pickel", "wb"))'''
